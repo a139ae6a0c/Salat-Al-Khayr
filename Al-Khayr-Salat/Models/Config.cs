9ce45b6cf96598c6cceb38e06288ddffdc79bd1f
أبو بخاري <71411025+a139ae6a0c@users.noreply.github.com>
2026-02-24 10:27:24 +0100

    Delete Al-Khayr-Salat directory
    faulty directory?

--- Al-Khayr-Salat/Models/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Al_Khayr_Salat.Functions;
-
-public class Config
-{
-    public string Mawaqit_URL { get; set; }
-    public int Volume { get; set; } = 50;
-    public string Adhan {get; set;}
-}
